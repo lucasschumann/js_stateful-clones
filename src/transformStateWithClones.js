e9d7a91af1c1547eec5f28006307e03f70e0345f
Lucas <soniclord123@gmail.com>
2025-02-09 11:26:58 -0300

    add modifications

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,27 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  const clone = { ...state };
+  let clone = { ...state };
   const finalArray = [];
 
   for (const action of actions) {
+    clone = { ...clone };
+
     if (action.type === 'addProperties') {
       Object.assign(clone, action.extraData);
-      finalArray.push({ ...clone });
+      finalArray.push(clone);
     }
 
     if (action.type === 'removeProperties') {
       for (const keyToRemove of action.keysToRemove) {
-        clone[keyToRemove] = undefined;
+        delete clone[keyToRemove];
       }
-      finalArray.push({ ...clone });
+      finalArray.push(clone);
     }
 
     if (action.type === 'clear') {
-      for (const key in clone) {
-        clone[key] = undefined;
-      }
-      finalArray.push({ ...clone });
+      clone = {};
+      finalArray.push(clone);
     }
   }
 
